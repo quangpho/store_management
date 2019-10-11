7ab59cf8b6e7b6e12b53bb4a895632ea7eb666b0
Quang Pho <quangphoduc@gmail.com>
2019-10-11 17:34:51 +0700

    Repository

--- StoreManagement.Repository/IRepository.cs
+++ StoreManagement.Repository/IRepository.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace StoreManagement.Repository
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(object input);
+        Task<T> Insert(T entity);
+        Task<T> Update(T entity);
+        Task Delete(T entity);
     }
 }
---
+++ StoreManagement.Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StoreManagement.DAL;
+
+namespace StoreManagement.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private StoreManagementContext _dbContext;
+        private DbSet<T> _dbSet;
+        public Repository(StoreManagementContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
+        public Task Delete(T entity)
+        {
+            
+        }
+
+        public Task<T> Get(object input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
